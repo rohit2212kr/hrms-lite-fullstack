f1b26d788700b032a28956023c613976b97a23a4
Rohit Kumar <pr1174682@gmail.com>
2026-02-07 22:30:19 +0530

    updated frontend proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://hrms-lite-fullstack-mjki.onrender.com',
         changeOrigin: true
       }
     }
